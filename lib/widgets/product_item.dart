7e095df62c008c772891787cbab8c6bc97dd0827
Tom Dinkelaker <tom.dinkelaker@gmail.com>
2020-04-30 16:38:20 +0200

    Consumer<Type> are an alternative way to bind to data from providers

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -8,39 +8,37 @@ import '../screens/product_details_screen.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Product product = Provider.of<Product>(context);
-
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        child: GestureDetector(
-          child: Image.network(product.imageUrl, fit: BoxFit.cover),
-          onTap: () {
-            Navigator.of(context).pushNamed(ProductDetailsScreen.routeName,
-                arguments: product.id);
-          },
-        ),
-        footer: GridTileBar(
-          backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(
-              product.isFavorite 
-                ? Icons.favorite
-                : Icons.favorite_border,
-              color: Theme.of(context).accentColor,
-            ),
-            onPressed: product.toggleFavoriteStatus,
+    return Consumer<Product>(
+      builder: (c, product, child) => ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          child: GestureDetector(
+            child: Image.network(product.imageUrl, fit: BoxFit.cover),
+            onTap: () {
+              Navigator.of(context).pushNamed(ProductDetailsScreen.routeName,
+                  arguments: product.id);
+            },
           ),
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-          ),
-          trailing: IconButton(
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading: IconButton(
               icon: Icon(
-                Icons.add_shopping_cart,
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
                 color: Theme.of(context).accentColor,
               ),
-              onPressed: () {}),
+              onPressed: product.toggleFavoriteStatus,
+            ),
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+            ),
+            trailing: IconButton(
+                icon: Icon(
+                  Icons.add_shopping_cart,
+                  color: Theme.of(context).accentColor,
+                ),
+                onPressed: () {}),
+          ),
         ),
       ),
     );
