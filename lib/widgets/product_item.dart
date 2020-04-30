e2764efa1c5c739bc5af2bd9d0265cf3435bbdcd
Tom Dinkelaker <tom.dinkelaker@gmail.com>
2020-04-30 16:42:37 +0200

    Consumer allows selective update by wrapping only those widgets that need to be updated dynamically (re-render optimization)

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -8,37 +8,39 @@ import '../screens/product_details_screen.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<Product>(
-      builder: (c, product, child) => ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: GridTile(
-          child: GestureDetector(
-            child: Image.network(product.imageUrl, fit: BoxFit.cover),
-            onTap: () {
-              Navigator.of(context).pushNamed(ProductDetailsScreen.routeName,
-                  arguments: product.id);
-            },
-          ),
-          footer: GridTileBar(
-            backgroundColor: Colors.black87,
-            leading: IconButton(
-              icon: Icon(
+    Product product = Provider.of<Product>(context, listen: false);
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          child: Image.network(product.imageUrl, fit: BoxFit.cover),
+          onTap: () {
+            Navigator.of(context).pushNamed(ProductDetailsScreen.routeName,
+                arguments: product.id);
+          },
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Consumer<Product>(
+              builder: (_, product, child) => Icon(
                 product.isFavorite ? Icons.favorite : Icons.favorite_border,
                 color: Theme.of(context).accentColor,
               ),
-              onPressed: product.toggleFavoriteStatus,
             ),
-            title: Text(
-              product.title,
-              textAlign: TextAlign.center,
-            ),
-            trailing: IconButton(
-                icon: Icon(
-                  Icons.add_shopping_cart,
-                  color: Theme.of(context).accentColor,
-                ),
-                onPressed: () {}),
+            onPressed: product.toggleFavoriteStatus,
           ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+              icon: Icon(
+                Icons.add_shopping_cart,
+                color: Theme.of(context).accentColor,
+              ),
+              onPressed: () {}),
         ),
       ),
     );
