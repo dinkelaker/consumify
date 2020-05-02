b11269868c81508471bdc8693968e32f6a89fd71
Tom Dinkelaker <tom.dinkelaker@gmail.com>
2020-05-02 17:39:43 +0200

    ImageUrl preview with listerners

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -12,6 +12,14 @@ class EditProductScreen extends StatefulWidget {
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+
+  @override
+  void initState() {
+    _imageFocusNode.addListener(_updateImageUrl);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,7 +64,46 @@ class _EditProductScreenState extends State<EditProductScreen> {
                   maxLines: 3,
                   keyboardType: TextInputType.multiline,
                   textInputAction: TextInputAction.next,
-                  focusNode: _descriptionFocusNode
+                  onFieldSubmitted: (_) {
+                    // when this input is submitted, go to Price input
+                    FocusScope.of(context).requestFocus(_imageFocusNode);
+                  },
+                  focusNode: _descriptionFocusNode,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      width: 100,
+                      height: 100,
+                      margin: EdgeInsets.only(top: 8, right: 10),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      child: Container(
+                        child: _imageUrlController.text.isEmpty
+                            ? Text('Enter a URL to an image')
+                            : FittedBox(
+                                child: Image.network(_imageUrlController.text,
+                                    fit: BoxFit.cover),
+                              ),
+                      ),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Image URL',
+                        ),
+                        textInputAction: TextInputAction.done,
+                        keyboardType: TextInputType.url,
+                        controller: _imageUrlController,
+                        focusNode: _imageFocusNode,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
@@ -65,5 +112,16 @@ class _EditProductScreenState extends State<EditProductScreen> {
       ),
       drawer: AppDrawer(),
     );
+  }
+
+  void _updateImageUrl() {
+    //this listener will only be called if imageUrl field had focus
+    if(!_imageFocusNode.hasFocus) {
+      //but only when we lost focus, we need to update
+      setState(() {
+        //we do not need to update state 
+        //since we know that the state updated
+      });
+    }
   }
 }
