29b0d3cdce0acaed12e8f69c9a5c45b862d3f497
Tom Dinkelaker <tom.dinkelaker@gmail.com>
2020-05-02 07:48:42 +0200

    CartItem widget in Cart
    Since there is two classes with name CartItem.
    One in Cart another now in CartItem.
    When using both in once class, we can either use:
    - import '../widgets/cart_item.dart' as ci;
      selecting prefix that can be used to indicate which class to use
      when instatiating a new one e.g. new ci.CartItem(...)
    - import '../providers/cart,dart' show Cart
      importing only the mentioned files in the show-list
      and filtering out all classes not mentioned (like CartItem).

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,41 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/cart.dart';
+import '../providers/cart.dart' show Cart;
+import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
 
   @override
   Widget build(BuildContext context) {
+    var cardData;
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Cart'),
       ),
-      body: Column(
-        children: <Widget>[
-          Card(
-            margin: EdgeInsets.all(15),
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              child: Row(
-                children: <Widget>[
-                  Text(
-                    'Title',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  SizedBox(width: 10),
-                  Consumer<Cart>(
-                    builder: (_, cartData, __) => Chip(
-                      label: Text('\$${cartData.totalAmount}'),
+      body: Consumer<Cart>(
+        builder: (_, cartData, __) => Column(
+          children: <Widget>[
+            Card(
+              margin: EdgeInsets.all(15),
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Total',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Spacer(),
+                    Chip(
+                      label: Text('\$${cartData.totalAmount}',
+                          style: TextStyle(
+                            color:
+                                Theme.of(context).primaryTextTheme.title.color,
+                          )),
                       backgroundColor: Theme.of(context).primaryColor,
                     ),
-                  ),
-                ],
+                    FlatButton(
+                      child: Text('ORDER NOW'),
+                      textColor: Theme.of(context).primaryColor,
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+            SizedBox(height: 10),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (ctxt, index) => CartItem(
+                    cartData.items.values.toList()[index].id,
+                    cartData.items.values.toList()[index].title,
+                    cartData.items.values.toList()[index].quantity,
+                    cartData.items.values.toList()[index].price,
+                ),
+                itemCount: cartData.items.length,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
