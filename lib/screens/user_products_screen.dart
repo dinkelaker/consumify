3deaa6a61a9bf9e649754ba153544d4f86765173
Tom Dinkelaker <tom.dinkelaker@gmail.com>
2020-05-08 06:59:24 +0200

    RefreshIndicator enables pull-to-refresh feature.
    Scrolling out of the border of a list
    will refresh its data source.

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -14,35 +14,41 @@ class UserProductsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Your Products!'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              Navigator.of(context).pushNamed(EditProductScreen.routeName);
-            },
-          )
-        ],
-      ),
-      body: Consumer<Products>(
-        builder: (_, productsData, __) => Padding(
-          padding: EdgeInsets.all(8),
-          child: ListView.builder(
-            itemCount: productsData.items.length,
-            itemBuilder: (_, index) => Column(
-              children: <Widget>[
-                UserProductItem(
-                    productsData.items[index].id,                    
-                    productsData.items[index].title,
-                    productsData.items[index].imageUrl),
-                Divider(),
-              ],
+        appBar: AppBar(
+          title: Text('Your Products!'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                Navigator.of(context).pushNamed(EditProductScreen.routeName);
+              },
+            )
+          ],
+        ),
+        body: Consumer<Products>(
+          builder: (_, productsData, __) => RefreshIndicator(
+            onRefresh: () => _refreshProducts(context),
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: ListView.builder(
+                itemCount: productsData.items.length,
+                itemBuilder: (_, index) => Column(
+                  children: <Widget>[
+                    UserProductItem(
+                        productsData.items[index].id,
+                        productsData.items[index].title,
+                        productsData.items[index].imageUrl),
+                    Divider(),
+                  ],
+                ),
+              ),
             ),
           ),
         ),
-      ),
-      drawer: AppDrawer()
-    );
+        drawer: AppDrawer());
+  }
+
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<Products>(context).fetchAndSetProducts();
   }
 }
