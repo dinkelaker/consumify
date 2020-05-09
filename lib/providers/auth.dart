9f32f8e0785684aec3ce1c95b0be0749dee66943
Tom Dinkelaker <tom.dinkelaker@gmail.com>
2020-05-09 09:45:26 +0200

    Login with Email using Firebase

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -10,6 +10,19 @@ class Auth with ChangeNotifier {
   DateTime _expiryData;
   String _userId;
 
+
+  Future<void> login(String email, String password) async {
+    const url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=$FIREBASE_WEB_API_KEY";
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {'email': email, 'password': password, 'returnSecureToken': true},
+      ),
+    );
+    print(json.decode(response.body));
+  }
+
   Future<void> signup(String email, String password) async {
     const url =
         "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$FIREBASE_WEB_API_KEY";
